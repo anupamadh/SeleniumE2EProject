62696999e3fe15d22a8e7c074bd5b308d73553b2
ANUPAMA DHIR <sabhssabhs@yahoo.com>
2020-04-23 14:45:12 +0800

    added code for login

--- src/test/java/Academy/HomePage.java
+++ src/test/java/Academy/HomePage.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.testng.annotations.Test;
 
 import pageObjects.LandingPage;
+import pageObjects.LoginPage;
 import resources.base;
 
 public class HomePage extends base{
@@ -13,6 +14,10 @@ public class HomePage extends base{
 		driver = initializeDriver();
 		driver.get("https://www.rahulshettyacademy.com/#/index");
 		LandingPage l= new LandingPage(driver);
-		l.getLogin().click();;
+		l.getLogin().click();
+		LoginPage lp = new LoginPage(driver);
+		lp.getEmail().sendKeys("abc123again@gmail.com");
+		lp.getPassword().sendKeys("abc123again");
+		lp.getLogin().click();
 	}
 }
