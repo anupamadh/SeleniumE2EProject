8b909225c11f29df936bce021320953227423d30
ANUPAMA DHIR <sabhssabhs@yahoo.com>
2020-04-23 15:10:43 +0800

    added code for TestNG dataprovider

--- src/test/java/Academy/HomePage.java
+++ src/test/java/Academy/HomePage.java
@@ -2,6 +2,7 @@ package Academy;
 
 import java.io.IOException;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageObjects.LandingPage;
@@ -9,15 +10,28 @@ import pageObjects.LoginPage;
 import resources.base;
 
 public class HomePage extends base{
-	@Test
-	public void basePageNavigation() throws IOException {
+	@Test(dataProvider="getData")
+	
+	public void basePageNavigation(String username, String password) throws IOException {
 		driver = initializeDriver();
 		driver.get("https://www.rahulshettyacademy.com/#/index");
 		LandingPage l= new LandingPage(driver);
 		l.getLogin().click();
 		LoginPage lp = new LoginPage(driver);
-		lp.getEmail().sendKeys("abc123again@gmail.com");
-		lp.getPassword().sendKeys("abc123again");
+		lp.getEmail().sendKeys(username);
+		lp.getPassword().sendKeys(password);
 		lp.getLogin().click();
+	}
+	
+	@DataProvider
+	public Object[][] getData() {
+		Object[][] data = new Object[1][2];
+		data[0][0] ="nonrestricteduser@qw.com";
+		data[0][1] ="123456";
+		
+	/**	data[1][0] ="restricteduser@qw.com";
+		data[1][1] ="456788";*/
+		
+		return data;
 	}
 }
