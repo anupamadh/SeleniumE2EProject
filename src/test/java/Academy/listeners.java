25f4be01b7ddf2f6e2ba8ae713c27ae4188c27fb
ANUPAMA DHIR <sabhssabhs@yahoo.com>
2020-04-26 09:39:30 +0800

    added code for screenshot on failure

---
+++ src/test/java/Academy/listeners.java
@@ -0,0 +1,21 @@
+package Academy;
+
+import java.io.IOException;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import resources.base;
+
+public class listeners implements ITestListener{
+base b = new base();
+
+public void onTestFailure(ITestResult result) {
+	try {
+		b.getScreenshot(result.getName());
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+}
